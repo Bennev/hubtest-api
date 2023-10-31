ce22805d342fa369d35c2d6f6cf63cc5d35995eb
Matheus Benevides <matheus.benevides@hublocal.com.br>
2023-10-31 13:52:08 -0300

    Some final adjusts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-// import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './infra/nestjs/modules/users/users.module';
-// import { typeOrmConfig } from './infra/nestjs/config/typeorm.config';
 import { CompaniesModule } from './infra/nestjs/modules/companies/companies.module';
 import { LocationsModule } from './infra/nestjs/modules/locations/locations.module';
 import { DatabaseModule } from './infra/nestjs/modules/database/database.module';
